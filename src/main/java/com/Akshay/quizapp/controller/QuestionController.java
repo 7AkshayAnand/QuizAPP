f65bd9a54882569209c96cc9f5e89d7ebfd463bb
AKshay Mausam <akshayjha560@gmail.com>
2025-05-14 21:18:02 +0530

    add remove and update method for question entity is implemented successfully

--- src/main/java/com/Akshay/quizapp/controller/QuestionController.java
+++ src/main/java/com/Akshay/quizapp/controller/QuestionController.java
@@ -3,10 +3,7 @@ package com.Akshay.quizapp.controller;
 import com.Akshay.quizapp.Question;
 import com.Akshay.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
@@ -23,4 +20,22 @@ public class QuestionController {
     public List<Question> getQuestionByCategory(@PathVariable String category){
         return questionService.getQuestionsByCategory(category);
     }
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question){
+      return questionService.addQuestion(question);
+
+    }
+
+    @PostMapping("update")
+    public String updateQuestion(@RequestBody Question question){
+        return questionService.updateQuestion(question);
+    }
+
+
+    @DeleteMapping("/remove/{id}")
+    public String removeQuestion(@PathVariable Integer id){
+        System.out.println("i am here");
+        return questionService.removeById(id);
+    }
+
 }
--- src/main/java/com/Akshay/quizapp/service/QuestionService.java
+++ src/main/java/com/Akshay/quizapp/service/QuestionService.java
@@ -4,8 +4,10 @@ import com.Akshay.quizapp.Question;
 import com.Akshay.quizapp.dao.QuestionDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuestionService {
@@ -23,5 +25,40 @@ public class QuestionService {
     public List<Question> getQuestionsByCategory(String category) {
 
        return questionDao.findByCategory(category);
+    }
+
+    public String addQuestion(Question question) {
+        questionDao.save(question);
+        return "Successfully created";
+    }
+
+    public String removeById(Integer id) {
+        System.out.println("deletion starts ");
+        questionDao.deleteById(id);
+        return "successfully deleted";
+    }
+
+
+    public String updateQuestion(Question updatedQuestion) {
+        Optional<Question> existingQuestionOpt = questionDao.findById(updatedQuestion.getId());
+
+        if (existingQuestionOpt.isPresent()) {
+//            System.out.println("yes i am present");
+            Question existingQuestion = existingQuestionOpt.get();
+
+            existingQuestion.setQuestionTitle(updatedQuestion.getQuestionTitle());
+            existingQuestion.setOption1(updatedQuestion.getOption1());
+            existingQuestion.setOption2(updatedQuestion.getOption2());
+            existingQuestion.setOption3(updatedQuestion.getOption3());
+            existingQuestion.setOption4(updatedQuestion.getOption4());
+            existingQuestion.setRightAnswer(updatedQuestion.getRightAnswer());
+            existingQuestion.setDiffcultylevel(updatedQuestion.getDiffcultylevel());
+            existingQuestion.setCategory(updatedQuestion.getCategory());
+
+             questionDao.save(existingQuestion);
+             return "updaed successfully";
+        } else {
+            throw new RuntimeException("Question not found with id: " + updatedQuestion.getId());
+        }
     }
 }
